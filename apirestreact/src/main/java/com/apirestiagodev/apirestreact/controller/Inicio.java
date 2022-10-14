c2b4c41096dee6508c661964c6baf62d383a0c51
tiagogiroto <tiago.giroto.neto1@gmail.com>
2022-10-13 21:16:28 -0300

    fx

--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/Inicio.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/Inicio.java
@@ -3,12 +3,15 @@ package com.apirestiagodev.apirestreact.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class Inicio {
   
-    @GetMapping(value="/")
+    @GetMapping(value="/", produces = "application/json")
+
     public String teste() {
-        return "ola, mmundo";
+        return "ola, mundo";
     }
     
 
