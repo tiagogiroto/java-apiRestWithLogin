ca28ea08e3acd228396345c1a914ae6347bdcfbb
tiagogiroto <tiago.giroto.neto1@gmail.com>
2022-10-14 10:22:48 -0300

    tentativa firebase, verificar key .json nao é buscado

--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/userController.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/userController.java
@@ -1,8 +1,11 @@
 package com.apirestiagodev.apirestreact.controller;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apirestiagodev.apirestreact.classFolder.user;
@@ -10,8 +13,8 @@ import com.apirestiagodev.apirestreact.repository.userRepository;
 
 @RestController
 public class userController {
-    
-     userRepository usuario = new userRepository();
+
+    userRepository usuario = new userRepository();
 
     @GetMapping(value="/userAll", produces = "application/json")
     public List<user> exibirUsuarios(){
@@ -19,5 +22,10 @@ public class userController {
 
     }
     
+    @GetMapping(value = "/getUser", produces="application/json") 
+    public user getUser(@RequestParam String name ) throws InterruptedException, ExecutionException{
+
+        return usuario.getUserRepository(name);
+    }
 
 }
--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/repository/userRepository.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/repository/userRepository.java
@@ -2,8 +2,14 @@ package com.apirestiagodev.apirestreact.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import com.apirestiagodev.apirestreact.classFolder.user;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
 
 public class userRepository {
     
@@ -27,5 +33,24 @@ public class userRepository {
         }
 
         return users;
+    }
+
+    public user getUserRepository(String name) throws InterruptedException, ExecutionException {
+
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection("users").document("name");
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot document = future.get();
+
+        user USER = null;
+
+        if(document.exists()) {
+            USER = document.toObject(user.class);
+            return USER;
+        }else {
+            return null;
+        }
+
     }
 }
