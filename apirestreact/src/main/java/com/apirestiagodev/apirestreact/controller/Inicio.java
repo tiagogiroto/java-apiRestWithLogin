4eb58745ccc80b3ce9de3273b66d2bee207c5f15
tiagogiroto <tiago.giroto.neto1@gmail.com>
2022-10-24 11:40:27 -0300

    progress

--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/Inicio.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/controller/Inicio.java
@@ -1,16 +1,11 @@
 package com.apirestiagodev.apirestreact.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 public class Inicio {
-  
-    @GetMapping(value="/", produces = "application/json")
-    public String teste() {
-        return "ola, mundo";
-    }
+
 }
 
