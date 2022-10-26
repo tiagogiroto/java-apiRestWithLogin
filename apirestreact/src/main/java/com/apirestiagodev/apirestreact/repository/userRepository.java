a15222ba3c0d442c8acb2c11a4b530c8d4f27ecd
tiagogiroto <tiago.giroto.neto1@gmail.com>
2022-10-26 08:57:47 -0300

    fix services user

--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/repository/userRepository.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/repository/userRepository.java
@@ -1,18 +1,15 @@
 package com.apirestiagodev.apirestreact.repository;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import com.apirestiagodev.apirestreact.classFolder.user;
+import com.apirestiagodev.apirestreact.service.userService;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 
@@ -24,24 +21,7 @@ public class userRepository {
         {Busca um  usuario por seu id} 
     */ 
     public user getUserRepository(String id) throws InterruptedException, ExecutionException {
-
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("users").document(id); // only 1 document
-
-        ApiFuture<DocumentSnapshot> future = documentReference.get();
-
-        DocumentSnapshot document = future.get();
-        System.out.println(document.getData());
-        user USER = null;
-
-        if(document.exists()) {
-            USER = document.toObject(user.class);
-
-            return USER;
-        }else {
-            return null;
-        }
-
+        return userService.getUserService(id);
     }
 
     /** 
@@ -49,20 +29,7 @@ public class userRepository {
         {buscar toda a lista de usuarios} 
     */  
     public List allUsers() throws InterruptedException, ExecutionException {
-
-        List allUsers = new ArrayList<>();
-
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<QuerySnapshot> future = dbFirestore.collection("users").get();
-        
-        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-
-
-        for (QueryDocumentSnapshot document : documents) {
-            allUsers.add(document.toObject(user.class));
-          }
-
-        return allUsers;
+        return userService.allUserService();
 
     }
 
@@ -71,23 +38,8 @@ public class userRepository {
         {Adiciona um novo usuario com id de documento aleatorio} 
     */  
     public Object addUser(String name, String trainningType) throws InterruptedException, ExecutionException{
-            Map<String, Object> docData = new HashMap<>();
-
-            System.out.println(name + " " + trainningType );
-        try {
-
-            docData.put("name", name);
-            docData.put("trainningType", trainningType);
-    
-            Firestore dbFirestore = FirestoreClient.getFirestore();
-            ApiFuture<WriteResult> future = dbFirestore.collection("users").document().set(docData);
-    
-            System.out.println("Update time : " + future.get().getUpdateTime());
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-       return docData;
+        return userService.addUser(name, trainningType);
+        
     }
 
     /** 
@@ -96,22 +48,7 @@ public class userRepository {
     */  
     public user alterUser() throws InterruptedException, ExecutionException{
 
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-
-        // referencia o documento pelo nome da coleção
-        // coloca a forma de identificacao
-        // altera pelo docref.upate, (nomeItem, alteraçãoNecessaria)
-            
-        DocumentReference docRef = dbFirestore.collection("users").document("XhHcTiMNzI9IXevylrPC");
-
-        // (async) Update one field
-        ApiFuture<WriteResult> future = docRef.update("trainningType", "teste alaateracao");
-
-        // ...
-        WriteResult result = future.get();
-        System.out.println("Write result: " + result);    
-
-        return null;
+        return userService.AlterUser();
     }
 
     /** 
@@ -119,22 +56,7 @@ public class userRepository {
         {deleta o usuario pelo id da coleção} 
     */  
     public String deleteUser(String id)throws InterruptedException, ExecutionException {
-
-        try {
-
-            Firestore dbFirestore = FirestoreClient.getFirestore();
-
-            ApiFuture<WriteResult> writeResult = dbFirestore.collection("users").document(id).delete(); 
-            // ...
-            System.out.println("Update time : " + writeResult.get().getUpdateTime());
-
-            return "Dados excluidos de users , com id = " + id;
-
-        } catch (Exception e) {
-            System.out.println(e);
-            // TODO: handle exception
-        }
-        return null;
+        return userService.deleteUserService(id);
         
     }
 
--- apirestreact/src/main/java/com/apirestiagodev/apirestreact/service/userService.java
+++ apirestreact/src/main/java/com/apirestiagodev/apirestreact/service/userService.java
@@ -1,14 +1,123 @@
 package com.apirestiagodev.apirestreact.service;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
+import org.springframework.stereotype.Service;
+
+import com.apirestiagodev.apirestreact.classFolder.user;
 import com.apirestiagodev.apirestreact.repository.userRepository;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
 
-@RestController
+@Service
 public class userService {
-    
+
     userRepository usuario = new userRepository();
+    List<user> user;
 
-    
+    public static user getUserService(String id) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection("users").document(id); // only 1 document
 
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot document = future.get();
+        System.out.println(document.getData());
+        user USER = null;
+
+        if (document.exists()) {
+            USER = document.toObject(user.class);
+
+            return USER;
+        } else {
+            return null;
+        }
+
+    }
+
+    public static List allUserService() throws InterruptedException, ExecutionException {
+
+        List allUsers = new ArrayList();
+
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = dbFirestore.collection("users").get();
+
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+
+        for (QueryDocumentSnapshot document : documents) {
+            allUsers.add(document.toObject(user.class));
+        }
+
+        return allUsers;
+    }
+
+    public static Object addUser(String name, String trainningType) throws InterruptedException, ExecutionException {
+
+        Map<String, Object> docData = new HashMap<>();
+
+        System.out.println(name + " " + trainningType);
+        try {
+
+            docData.put("name", name);
+            docData.put("trainningType", trainningType);
+
+            Firestore dbFirestore = FirestoreClient.getFirestore();
+            ApiFuture<WriteResult> future = dbFirestore.collection("users").document().set(docData);
+
+            System.out.println("Update time : " + future.get().getUpdateTime());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return docData;
+    }
+
+    public static user AlterUser() throws InterruptedException, ExecutionException{
+
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        // referencia o documento pelo nome da coleção
+        // coloca a forma de identificacao
+        // altera pelo docref.upate, (nomeItem, alteraçãoNecessaria)
+            
+        DocumentReference docRef = dbFirestore.collection("users").document("l4dV3ObfLBReWgHdUj2W");
+
+        // (async) Update one field
+        ApiFuture<WriteResult> future = docRef.update("name", "irmaozada");
+
+        // ...
+        WriteResult result = future.get();
+        System.out.println("Write result: " + result);    
+
+        return null;
+
+    }
+
+    public static String deleteUserService(String id) throws InterruptedException, ExecutionException {
+        try {
+
+            Firestore dbFirestore = FirestoreClient.getFirestore();
+
+            ApiFuture<WriteResult> writeResult = dbFirestore.collection("users").document(id).delete(); 
+            // ...
+            System.out.println("Update time : " + writeResult.get().getUpdateTime());
+
+            return "Dados excluidos de users , com id = " + id;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            // TODO: handle exception
+        }
+        return null;
+    }
 }
